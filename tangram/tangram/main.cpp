43a26949b51511b7c27b918122bec3ca35f6c93c
Ethan Christopher Barnes <barnes.c.ethan@gmail.com>
2021-10-29 10:54:48 +0100

    Attempting to colour quadralaterals

--- tangram/tangram/main.cpp
+++ tangram/tangram/main.cpp
@@ -57,8 +57,10 @@ void makeSquare(GLfloat arr[], GLuint indices[], int index) {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(newArr), newArr, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[eboIndex]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(newIndices), newIndices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
@@ -151,10 +153,10 @@ int main(void)
 
 	GLfloat square[] = {
 		// Position data,    colour data
-		0.3f, 0.3f, 0.0f,  // Top
-		0.2f, 0.2f, 0.0f, // Left
-		0.4f, 0.2f, 0.0f, // Right
-		0.3f, 0.1f, 0.0f, //Bottom
+		0.3f, 0.3f, 0.0f, 1.0f, 0.0f, 1.0f,  // Top
+		0.2f, 0.2f, 0.0f, 1.0f, 0.0f, 1.0f, // Left
+		0.4f, 0.2f, 0.0f, 1.0f, 0.0f, 1.0f, // Right
+		0.3f, 0.1f, 0.0f, 1.0f, 0.0f, 1.0f, //Bottom
 	};
 
 	GLuint indices[] = {
@@ -164,10 +166,10 @@ int main(void)
 
 	GLfloat parallelogram[] = {
 		// Position data,    colour data
-		0.1f, 0.1f, 0.0f,  // Top
-		0.0f, 0.0f, 0.0f, // Left
-		0.3f, 0.1f, 0.0f, // Right
-		0.2f, 0.0f, 0.0f, //Bottom
+		0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f,  // Top
+		0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, // Left
+		0.3f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f, // Right
+		0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, //Bottom
 	};
  
 
@@ -233,7 +235,16 @@ int main(void)
 		GLint transformLoc = glGetUniformLocation(shaderProgram, "transform");
 
 		drawTriangle(transform, 0);
-		drawTriangle(transform, 1);
+		/* animation example: triangle translation */
+		glm::mat4 transform1;
+		if ((GLfloat)glfwGetTime() * 0.1f > 0.5 && (GLfloat)glfwGetTime() * 0.1f <= 1) {
+			transform1 = glm::translate(transform, glm::vec3((GLfloat)glfwGetTime() * 0.1f, 0.0f, 0.0f));
+		}
+		else {
+			//transform1 = glm::translate(transform, glm::vec3(0.5f, 0.0f, 0.0f));
+		}
+
+		drawTriangle(transform1, 1);
 		drawTriangle(transform, 2);
 		drawTriangle(transform, 3);
 		drawTriangle(transform, 4);
